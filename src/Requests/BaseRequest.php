d87252bd56b7605ecaaf4c13a879fcdf1463ab40
Nick Coffey <ncoffey@ripstech.com>
2017-08-04 09:37:05 -0400

    Add method to handle responses and throw exceptions to BaseRequest

--- src/Requests/BaseRequest.php
+++ src/Requests/BaseRequest.php
@@ -3,6 +3,9 @@
 namespace RIPS\APIConnector\Requests;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
+use RIPS\APIConnector\Exceptions\ClientException;
+use RIPS\APIConnector\Exceptions\ServerException;
 
 abstract class BaseRequest
 {
@@ -17,5 +20,18 @@ abstract class BaseRequest
     public function __construct(Client $client)
     {
         $this->client = $client;
+    }
+
+    protected function handleResponse(Response $response)
+    {
+        $statusCode = (int) floor($response->getStatusCode() / 100);
+
+        if ($statusCode === 4) {
+            throw new ClientException($response->getBody(), $response->getStatusCode());
+        } elseif ($statusCode === 5) {
+            throw new ServerException($response->getBody(), $response->getStatusCode());
+        }
+
+        return json_decode($response->getBody());
     }
 }
