e961b41fcc960ec58481b85f9870f09e187d8f04
Hendrik Buchwald <hbuchwald@ripstech.com>
2019-10-30 17:59:34 +0100

    Add artifacts

--- test/Requests/ApplicationRequestsTest.php
+++ test/Requests/ApplicationRequestsTest.php
@@ -3,6 +3,7 @@
 namespace RIPS\Test\Requests;
 
 use RIPS\Connector\Requests\Application\AclRequests;
+use RIPS\Connector\Requests\Application\ArtifactRequests;
 use RIPS\Connector\Requests\Application\ProfileRequests;
 use RIPS\Connector\Requests\Application\ScanRequests;
 use RIPS\Connector\Requests\Application\UploadRequests;
@@ -173,5 +174,15 @@ class ApplicationRequestsTest extends TestCase
         $uploadRequests = $this->applicationRequests->uploads();
 
         $this->assertInstanceOf(UploadRequests::class, $uploadRequests);
+    }
+
+    /**
+     * @test
+     */
+    public function artifacts()
+    {
+        $artifactRequests = $this->applicationRequests->artifacts();
+
+        $this->assertInstanceOf(ArtifactRequests::class, $artifactRequests);
     }
 }
